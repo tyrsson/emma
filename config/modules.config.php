908827a262cfe70eca84bb80af5f320d1f0e4951
Emmi05 <florescarmona.emma@utacapulco.edu.mx>
2023-12-31 10:53:39 -0600

    First form fetch

--- config/modules.config.php
+++ config/modules.config.php
@@ -6,6 +6,7 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    //'Webinertia\Utils',
     'Laminas\Navigation',
     'Webinertia\Filter',
     'Webinertia\Validator',
